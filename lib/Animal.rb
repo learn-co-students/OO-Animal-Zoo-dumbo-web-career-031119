76f2316fe61900dd6759420dedba45bd12fd4c63
Jordan Rivera <jordan.rivera34@gmail.com>
2019-03-14 17:45:40 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Animal
 
+    attr_reader :species, :nickname
+    attr_accessor :weight, :zoo
+    
+
+    @@animals = []
+    
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @zoo = zoo
+        @@animals << self
+     
+    end
+
+
+    def self.all
+        @@animals
+        
+    end
+
+    def self.find_by_species(species)
+        self.all.select do |animal|
+        animal.species == species
+        end
+    end
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,48 @@
 class Zoo
 
+    attr_reader :name, :location
+    
+        @@zoos = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@zoos << self.location
+    end
+
+
+    def self.all
+       @@zoos
+       # inside a class method, self is the class (Animal)
+    end
+
+
+    def animals
+        # inside an instance method, self is the instance who is calling that method 
+        Animal.all.select do |animal| 
+        self == animal.zoo
+        end
+    end
+
+    def animal_species
+        self.animals.map{|animal| animal.species}.uniq
+    end
+
+    def find_by_species(arg)
+        self.animals.select do |animal|
+            animal.species == arg
+        end
+    end 
+
+    def animal_nicknames
+        animals.map {|animal| animal.nickname}
+
+    end
+
+    def self.find_by_location(locationArg)
+        @@all.select do |zoo|
+            zoo.location == locationArg
+        end
+    end
+
 end
