e35feea3654a6974e67b1ef08b4ee3d67bc2d30b
Jeff Yu <jwy5140@gmail.com>
2019-03-14 17:02:52 -0400

    Too many zooz

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,27 @@
 class Animal
 
+    @@all = Array.new
+    @@find_by_species = Array.new
+
+    attr_reader :species, :nickname, :find_by_species
+    attr_accessor :weight, :zoo
+
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        @@find_by_species = @@all.select {|x|
+            x.species == species
+        }
+    end
+
 end
+
