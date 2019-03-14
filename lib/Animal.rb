3753a3fa6214207a1b789e75e7c30d0cd49a95c2
Jerry Velazquez <jvr572@gmail.com>
2019-03-14 15:37:04 -0400

    not complete

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,7 +1,25 @@
 class Animal
+  attr_reader :species, :nickname
+  attr_accessor :weight
 
-   def initialize
-     
+  ALLANIMALS = []
 
+   def initialize(species, weight, nickname, zoo)
+     @species = species
+     @nickname = nickname
+     @weight =  weight
+     @zoo = zoo
+     ALLANIMALS << self
+   end
+
+   def self.all
+     ALLANIMALS
+   end
+
+   def self.find_species_by(animal)
+     ALLANIMALS.select do |ani|
+       if ani == animal
+       end
+      end
    end
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,8 +1,51 @@
 class Zoo
+  attr_reader :location, :name
 
-  def initialize
-    @zoo = zoo 
+  ALLZOOS = []
+  def initialize(location, name)
+    @location = location
+    @name = name
+    ALLZOOS << self
+  end
+
+  def self.all
+    ALLZOOS
+  end
+
+  def animals
+    Animal.all.select do |ani|
+      ani.zoo == self
+    end
+  end
+
+  def add_animal(species, weight, nickname)
+    Animal.new(species, weight, nickname, self)
+  end
+
+  def animal_species
+    arr = []
+    animals.select do |ani|
+      if arr == []
+        arr << ani
+      else
+        does_exist = false
+        arr.each do |x|
+          if x.species == ani.species
+            does_exist = true
+          end
+        end
+        if(!does_exist)
+          arr << ani
+        end
+      end
+    end
+
 
   end
 
+  def find_by_species(species)
+    animals.select do |ani|
+      ani.species == ani
+    end
+  end
 end
